bb2321f35721a01d9898a31b470c2b840ebe4bba
Alice Roberts <alice.groberts12@gmai.com>
2023-12-13 12:14:03 -0600

    Canvas Added

---
+++ Assets/_Game/Scripts/PlayerInfo.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour
+{
+    [Header("General Stats")]
+    [SerializeField] private int _health;
+    [SerializeField] private int _stamina;
+
+    [Header("Combat Stats")]
+    [SerializeField] private int _attack;
+    [SerializeField] private int _defense;
+    
+
+    
+}
