dfc6facec0772705da8e9493357745695ed9bf3f
Alice Roberts <alice.groberts12@gmai.com>
2023-12-13 13:14:27 -0600

    Player Script

--- Assets/_Game/Scripts/PlayerInfo.cs
+++ Assets/_Game/Scripts/PlayerInfo.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PlayerInfo : MonoBehaviour
 {
     [Header("General Stats")]
@@ -19,13 +21,57 @@ public class PlayerInfo : MonoBehaviour
     private int _currentHealth;
     private int _currentStamina;
 
-    private void Awake()
+    private void Start()
     {
         _currentHealth = _maxHealth;
         _currentStamina = _maxStamina;
-
+        _healthBar.SetMaxHealth(_maxHealth);
+        _staminaBar.SetMaxStamina(_maxStamina);
     }
 
+    public void TakeDamage(int damage)
+    {
+        _currentHealth -= damage;
+        _healthBar.SetHealth(_currentHealth);
+    }
+
+    public void WhenSprint()
+    {
+        _currentStamina -= 5;
+        _staminaBar.SetStamina(_currentStamina);
+    }
+
+    public void LevelUp()
+    {
+        _maxHealth += 10;
+        _maxStamina += 5;
+        _maxAttack += 5;
+        _maxDefense += 2;
+
+        _healthBar.SetMaxHealth(_maxHealth);
+        _staminaBar.SetMaxStamina(_maxStamina);
+        _levelNumber.IncreaseLevelValue();
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TakeDamage(5);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            WhenSprint();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            LevelUp();
+        }
+
+    }
 
 
 }
